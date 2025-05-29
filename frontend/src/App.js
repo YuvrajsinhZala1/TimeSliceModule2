98f0b62e742557cb5057200e978cfe39548c4013
unknown <yuvrajsinhzala1>
2025-05-29 17:24:41 +0530

    Fixed my Frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,11 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
 import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
